8e5297a9de132a84f9ac8cf5c5072fb9b698df72
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-03-22 16:08:43 +0200

    Add files via upload

---
+++ TestDummy/DummyTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void LosesHealthIfAttack()
+        {
+            Dummy target = new Dummy(10, 10);
+
+            target.TakeAttack(10);
+
+            Assert.AreEqual(0, target.Health);
+        }
+
+        [Test]
+        public void DeadDummyThrowsExceptionIfAttack()
+        {
+            Dummy target = new Dummy(0, 10);
+
+            Assert.Throws<InvalidOperationException>(() => target.TakeAttack(2)); 
+        }
+
+        [Test]
+        public void IFDeadDummyGivesExp()
+        {
+            Dummy target = new Dummy(0, 10);
+
+            Assert.AreEqual(10, target.GiveExperience());
+        }
+
+        [Test]
+        public void IFAliveDummyDoesNotGiveExp()
+        {
+            Dummy target = new Dummy(1, 10);
+
+            Assert.Throws<InvalidOperationException>(() => target.GiveExperience());
+        }
+    }
+}
